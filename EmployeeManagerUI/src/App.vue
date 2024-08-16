a6b4d7f5ca8d1e9dc34ec0f4782c7ff8ab55eb68
chizuruchi <827509758@qq.com>
2024-08-16 14:03:06 +0800

    Resolve merge conflicts

---
+++ EmployeeManagerUI/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data: function(){
+    return{
+      message: "hello vue!"
+    };
+  }
+  
+}
+</script>
+
+<style>
+
+</style>
